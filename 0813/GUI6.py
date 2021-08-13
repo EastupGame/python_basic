c71e15163a6726d29409c5153484a1f7f6ea5b8d
EastupGame <69027539+EastupGame@users.noreply.github.com>
2021-08-13 14:21:00 +0900

    Add files via upload

--- 0813/GUI6.py
+++ 0813/GUI6.py
@@ -1,4 +1,10 @@
+import random
 import tkinter
+
+
+def click_btn():
+    label["text"] = random.choice(["1","2","3","4","5"])
+    label.update()
 
 root = tkinter.Tk()
 root.resizable(False, False)
@@ -7,9 +13,10 @@ canvas.pack()
 player = tkinter.PhotoImage(file = "player.png")
 canvas.create_image(200,300, image = player )
 
-label = tkinter.Label(text = "??", font = ("System", 120))
+label = tkinter.Label(root, text = "??", font = ("System", 120))
 label.place(x = 380, y = 60)
 
-button = tkinter.Button(text = "슛 넣을 확률", font = ("System", 40))
+button = tkinter.Button(root, text = "숫자카드를 뽑아주세요.",
+                        font = ("System", 40), command = click_btn )
 button.place(x = 380, y = 480)
 root.mainloop()
